65696550d80543b94df7d26dd500bb89463aa7ae
Saena <toastyhatesthesea@gmail.com>
2020-09-17 16:42:54 -0700

    Added pointers code function

---
+++ Pointers/pointers.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void update(int *a, int *b)
+{
+    int c = *a;
+    *a = *a + *b;
+    // Complete this function
+    *b = c - *b;
+
+    if (*b < 0)
+    {
+        *b = *b * -1;
+    }
+
+    //printf("%s\n", *a);
+    //printf("%s", *b);
+}
+
+int main()
+{
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
